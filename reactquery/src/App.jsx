a11cf36eaff602b7d7e279df2beeb200947534cf
devanshV18 <devanshverma024@gmail.com>
2024-04-18 19:46:35 +0530

    api functions done

--- reactquery/src/App.jsx
+++ reactquery/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {useQuery} from '@tanstack/react-query'
 
 import './App.css'
 
 function App() {
 
+  useQuery
 
   return (
     <div>React Query</div>
--- reactquery/src/main.jsx
+++ reactquery/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
+const queryClient = new QueryClient() //new instance of react query
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      {/* wrapping the app using QCP specifying that client is queryClient */}
+    <QueryClientProvider client={queryClient}>  
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
