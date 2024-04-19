bb62c275bf1d42db9f1614eead5cc9bd13e16473
devanshV18 <devanshverma024@gmail.com>
2024-04-19 17:49:37 +0530

    fetch using react query done

--- reactquery/src/main.jsx
+++ reactquery/src/main.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
-const queryClient = new QueryClient() //new instance of react query
+// Create a new instance of QueryClient
+const queryClient = new QueryClient()
 
+// Wrap your app with QueryClientProvider and provide the queryClient instance
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      {/* wrapping the app using QCP specifying that client is queryClient */}
-    <QueryClientProvider client={queryClient}>  
+    <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false}/>
     </QueryClientProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
